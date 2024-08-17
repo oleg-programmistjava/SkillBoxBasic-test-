af1b4900c117702454e33e3562743fce264cc025
oleg-programmistjava <programmistjava@yandex.ru>
2024-08-17 14:32:21 +0300

    Создал каркас приложения:  - создал шапку программы;  - усовершенствовал логику метода printAllArithmetics (теперь он показывает, что такое сумма и что такое произведение, а также улучшил визуальное отображение сравнения передаваемых чисел в программу);  - сделал более привлекательным завершение работы программы.

--- 04_ObjectsAndClasses_part1/04-01_ArithmeticNumber/src/Main.java
+++ 04_ObjectsAndClasses_part1/04-01_ArithmeticNumber/src/Main.java
@@ -3,17 +3,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
 
         int inputA = 0;
         int inputB = 0;
 
+        System.out.println("ПРОГРАММА ДЛЯ РАСЧЕТА СУММЫ, ПРОИЗВЕДЕНИЯ ЧИСЕЛ, а также");
+        System.out.println("КАКОЕ ИЗ ПЕРЕДАВАЕМЫХ ЧИСЕЛ - БОЛЬШЕ ИЛИ МЕНЬШЕ, либо ОНИ РАВНЫ");
+        System.out.println("---");
+        System.out.print("Для продолжения нажмите ENTER ");
+        keyboard.nextLine();
+
+        System.out.println();
+
         Arithmetic generalArithmetic = new Arithmetic(inputA, inputB);
 
-        System.out.print("Укажите первое число: ");
+        System.out.println("ВВОД ИСХОДНЫХ ДАННЫХ:");
+
+        System.out.print(" - Укажите первое число: ");
         inputA = new Scanner(System.in).nextInt();
         generalArithmetic.a = inputA;
 
-        System.out.print("Укажите второе число: ");
+        System.out.print(" - Укажите второе число: ");
         inputB = new Scanner(System.in).nextInt();
         generalArithmetic.b = inputB;
 
@@ -22,6 +33,12 @@ public class Main {
 
         System.out.println();
         generalArithmetic.printAllArithmetics();
+
+        System.out.println();
+
+        System.out.println("===========================");
+        System.out.println("    ПРОГРАММА ЗАВЕРШЕНА    ");
+        System.out.println("===========================");
     }
 
 }
